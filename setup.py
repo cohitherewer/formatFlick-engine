f86ae43ccf846c4345a350a981cce32558ea4740
Abhinaba Chakraborty <abhinabacr4@gmail.com>
2023-12-13 00:40:25 +0530

    completed setup
    Completed Setup, uploaded the module into testpypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='formatflick',
+    version='0.1.0',    
+    description='Python module for interchanging file formats',
+    url='https://github.com/cohitherewer/formatFlick-engine',
+    author='Abhinaba Chakraborty',
+    author_email='abhinabacr4@gmail.com',
+    license='BSD 3-clause',
+    packages=['formatflick'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'
+    ],
+)
